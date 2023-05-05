0b9bb8754dfe552cabb65e13366599242c411791
Takuma Kira <takuma@kirakiraworx.com>
2023-05-06 00:16:14 +0900

    Use C++ library from flutter

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -1,8 +1,19 @@
 
 import 'native_add_platform_interface.dart';
 
+import 'dart:ffi'; // For FFI
+import 'dart:io'; // For Platform.isX
+
 class NativeAdd {
   Future<String?> getPlatformVersion() {
     return NativeAddPlatform.instance.getPlatformVersion();
   }
 }
+
+final DynamicLibrary nativeAddLib = Platform.isAndroid
+    ? DynamicLibrary.open('libnative_add.so')
+    : DynamicLibrary.process();
+
+final int Function(int x, int y) nativeAdd = nativeAddLib
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_add')
+    .asFunction();
